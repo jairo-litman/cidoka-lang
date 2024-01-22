9db685992abce8ed989fe5d924c78bcce35a4c3c
Jairo Litman <109396323+JB-Litman@users.noreply.github.com>
2024-01-21 22:13:43 -0300

    rppl

--- repl/repl.go
+++ repl/repl.go
@@ -2,7 +2,7 @@ package repl
 
 import (
 	"boludolang/lexer"
-	"boludolang/token"
+	"boludolang/parser"
 	"bufio"
 	"fmt"
 	"io"
@@ -15,15 +15,28 @@ func Start(in io.Reader, out io.Writer) {
 
 	for {
 		fmt.Print(PROMPT)
-		scanned := scanner.Scan() // read a line of input from the user
+		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
 
-		line := scanner.Text() // get the line of input
-		lex := lexer.New(line) // create a lexer for the input
-		for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
-			fmt.Printf("%+v\n", tok) // print the tokens
+		line := scanner.Text()
+		lex := lexer.New(line)
+		parser := parser.New(lex)
+		program := parser.ParseProgram()
+
+		if len(parser.Errors()) != 0 {
+			printParserErrors(out, parser.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
