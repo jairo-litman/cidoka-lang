d6455eae22a5231c014966fda96eafcdd27074d4
Jairo Litman <130161309+jairo-litman@users.noreply.github.com>
2024-02-18 12:33:23 -0300

    added examples and flag to run code from files

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"cidoka/compiler"
+	"cidoka/evaluator"
+	"cidoka/lexer"
+	"cidoka/object"
+	"cidoka/parser"
 	"cidoka/repl"
+	"cidoka/vm"
 	"flag"
 	"fmt"
 	"os"
@@ -9,9 +15,15 @@ import (
 )
 
 var engine = flag.String("engine", "vm", "use 'vm' or 'eval'")
+var input = flag.String("input", "", "input file")
 
 func main() {
 	flag.Parse()
+
+	if *input != "" {
+		runFile(*input)
+		return
+	}
 
 	user, err := user.Current()
 	if err != nil {
@@ -21,4 +33,41 @@ func main() {
 	fmt.Printf("Running in %s mode\n", *engine)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout, *engine)
+}
+
+func runFile(input string) {
+	f, err := os.ReadFile(input)
+	if err != nil {
+		panic(err)
+	}
+
+	var result object.Object
+
+	l := lexer.New(string(f))
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	if *engine == "vm" {
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Printf("compiler error: %s", err)
+			return
+		}
+
+		machine := vm.New(comp.Bytecode())
+
+		err = machine.Run()
+		if err != nil {
+			fmt.Printf("vm error: %s", err)
+			return
+		}
+
+		result = machine.LastPoppedStackElem()
+	} else {
+		env := object.NewEnvironment()
+		result = evaluator.Eval(program, env)
+	}
+
+	fmt.Printf("engine=%s, result=%s\n", *engine, result.Inspect())
 }
